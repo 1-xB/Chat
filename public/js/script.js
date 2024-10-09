ec1982e6d9e52c0010c022b67e4c7941d4c89197
xB0bon <pan.bobon@gmail.com>
2024-10-09 20:18:42 +0200

    Added user is typing information, bind send message and join room on enter, users list in a room, fixed disconnecting from server, fixed username login security.

--- public/js/script.js
+++ public/js/script.js
@@ -10,49 +10,47 @@ const user_name_input = document.getElementById("nickName");
 const room_input = document.getElementById("room");
 const room_code_span = document.getElementById("room-code");
 const notification_on_user_disconnect = document.getElementById("notification-disconnect");
+const html_users_in_room_display = document.getElementById("users-list");
+const html_send_menu = document.getElementById("send-menu")
 let messageWidth;
 let username; // nazwa użytkownika
 let room = "public";
-socket.emit("join-room",room)
 
 let isLogged = false;
 let isJoined = false;
+
+
+let isTyping = false;
 
 // po połączeniu z serverem.
 socket.on('connect' , ()=>{
     console.log(`Connected as: ${socket.id}`)
 })
+//SOCKET.ON
 
-//ustawianie i zazpisywanie nazwy uzytkownika na serwerze
-function LogIn() {
-    username = user_name_input.value;
-    if (!username) {
-        username = "guest";
-    }
-    room = "public"
-    room_input.value = room;
-    room_code_span.textContent = room;
-    isJoined = true;
-
-    user_info.textContent = `Username: ${username}`;
-    socket.emit("log-in", username);
-}
-//wyswietlanie id uzytkownika
-// socket.on('display-id',(id)=>{
-//     user_id.textContent += id;
-// })
+// odebranie listy pokoi
+socket.on("room-list", (list)=>{
+    html_users_in_room_display.innerHTML = "";
+    list.forEach(user=>{
+        const p = document.createElement("div")
+        html_users_in_room_display.append(p)
+        p.classList.add("list-username")
+        p.textContent = user
+    })
+})
 
 //zejęta nazwa użytkownika
 socket.on("wrong-username" ,(username)=>{
     warning.textContent = "Nazwa użytkownika już istnieje"
-    username = null
     user_name_input.value = ""
     isLogged = false;
     isJoined = false;
 })
+
 //dobra nazwa użytkownika
 socket.on("correct-username", ()=>{
     isLogged = true;
+    socket.emit("join-room",room,username)
 })
 
 // odebranie wiadomości
@@ -80,22 +78,65 @@ socket.on("message", (msg,username) => {
     chatDiv.scrollTop = chatDiv.scrollHeight;
 })
 
+// powiadomienie o rozłączeniu użytkownika
+socket.on("disconnected-user",()=>{
+    notification_on_user_disconnect.textContent = `user ${name} disconnected`
+    notification_on_user_disconnect.style.width = "100%"
+    notification_on_user_disconnect.style.backgroundColor = "red"
+})
+
+socket.on("user-is-typing-display" , (username)=>{
+    const m = document.createElement("div")
+    html_send_menu.append(m);
+    m.classList.add("isTyping")
+    m.classList.add(username)
+    m.textContent = username + " is typing"
+})
+
+socket.on("user-stopped-typing-display",(username)=>{
+    document.getElementsByClassName(username)[0].remove();
+})
+
+// FUNCKJE
+
+//ustawianie i zazpisywanie nazwy uzytkownika na serwerze
+function LogIn() {
+    username = user_name_input.value;
+    if (!username) {
+        username = "guest";
+    }
+    room = "public"
+    room_input.value = room;
+    room_code_span.textContent = room;
+    isJoined = true;
+
+
+
+    user_info.textContent = `Username: ${username}`;
+    socket.emit("log-in", username);
+}
+//wyswietlanie id uzytkownika
+// socket.on('display-id',(id)=>{
+//     user_id.textContent += id;
+// })
+
+
 // funkcja dołączania do pokoju
 function joinRoom(){
     if (!isLogged) {
         warning.textContent = "You are not logged in!"
         return
     }
+
+
+    socket.emit("leave-room",room,username)
     room = room_input.value;
-    socket.emit("join-room",room)
+    socket.emit("join-room",room,username)
     isJoined = true;
     room_code_span.textContent = room;
 }
-socket.on("disconnected",name=>{
-    notification_on_user_disconnect.textContent = `user ${name} disconnected`
-    notification_on_user_disconnect.style.width = "100%"
-    notification_on_user_disconnect.style.backgroundColor = "red"
-})
+
+
 
 // funkcja wysyłania wiadomości
 function sendMessage() {
@@ -154,3 +195,28 @@ function sendMessage() {
     // czyszczenie inputa
     message_input.value = "";
 }
+
+// sprawdzanie czy message_input jest aktywny
+message_input.addEventListener("focus", () => {
+    isTyping = true;
+    socket.emit("user-is-typing" ,username, room)
+})
+
+// sprawdzanie czy message_input jest nieaktywny
+message_input.addEventListener("blur", () => {
+    isTyping = false;
+    socket.emit("user-stopped-typing",username,room)
+})
+
+// bind enter do wysyłania wiadomości.
+message_input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" && isTyping) {
+        sendMessage()
+    }
+})
+
+room_input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" && isTyping) {
+        sendMessage()
+    }
+})
--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
 let users = {}; //użytkownicy
+let rooms = {};
+
+let temp_name;
 
 // serwowanie plików statycznych z katalogu 'public'
 app.use(express.static('public'));
@@ -34,7 +36,8 @@ io.on("connection", (socket => {
         else {
             // dodanie użytkownika do listy
             users[username] = {
-                id:socket.id
+                id:socket.id,
+                room: "public",
             }
 
             // wysłanie informacji o poprawnym zalogowaniu
@@ -43,11 +46,36 @@ io.on("connection", (socket => {
     });
 
     // dołączanie do pokoju
-    socket.on("join-room",room=>{
+    socket.on("join-room",(clientroom, username)=>{
+
 
         // dołączenie do pokoju
-        socket.join(room)
-        //socket.to(room).emit("send-message", "Użytkownik dołączył do pokoju")
+        socket.join(clientroom)
+
+        // dodanie użytkownika do listy
+        if (!rooms[clientroom]) {
+            rooms[clientroom] = []
+        }
+        rooms[clientroom].push(username)
+        if (users[username]) {
+            users[username].room = clientroom
+        }
+
+
+        socket.to(clientroom).emit("send-message", "Użytkownik dołączył do pokoju")
+        io.to(clientroom).emit("room-list",rooms[clientroom])
+
+        console.log("Użytkownik " + username + " dołączył do pokoju " + clientroom)})
+
+
+    socket.on("leave-room" , (room,username)=>{
+        if(rooms[room].includes(username)){
+            // opuszczenie pokoju
+            rooms[room] = rooms[room].filter(user => user !== username)
+            console.log("Użytkownik " + username + " opuścił pokój " + room)
+            // wysłanie nowej listy uzytkowników w pokoju
+            io.to(room).emit("room-list",rooms[room])
+        }
     })
 
     // wysyłanie wiadomości
@@ -56,19 +84,30 @@ io.on("connection", (socket => {
         socket.broadcast.to(room).emit("message", message, username)
     })
 
+    // wysyłanie informacji o pisaniu
+    socket.on("user-is-typing", (username, room)=>{
+        socket.to(room).emit("user-is-typing-display",username)
+    })
+
+    socket.on("user-stopped-typing" ,(username,room)=>{
+        socket.to(room).emit("user-stopped-typing-display",username)
+    })
+
     // rozłączenie użytkownika
     socket.on("disconnect", () => {
         // usunięcie użytkownika z listy
         for (let name in users) {
             if (users[name].id === socket.id) {
-                socket.emit("disconnected",name)
+                temp_name = name;
+                // opuszczenie pokoju
+                rooms[users[name].room] = rooms[users[name].room].filter(user => user !== name)
+                io.to(users[name].room).emit("room-list",rooms[users[name].room])
+                // wysłanie informacji o rozłączeniu
                 delete users[name];
                 break;
             }
         }
     })
-
-
 }))
 
 
